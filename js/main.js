6d3748908ee25a9991152c7afac6e534fc5dc0e8
Leon <leeleonfly@gmail.com>
2018-04-16 10:12:05 -0700

    Add ARIA

--- js/main.js
+++ js/main.js
@@ -158,6 +158,8 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role', 'button');
+  more.setAttribute('aria-label', 'View Details for ' + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
